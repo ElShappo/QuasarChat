b5926b921ed311287d252f964915b782d6dc94a5
Deni Shaipov <deni.shaipp@gmail.com>
2023-06-27 16:41:22 +0300

    chat page done for now

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,6 @@
     <q-header elevated>
       <q-toolbar>
         <q-btn
-          v-if="$route.fullPath.includes('/chat')"
           @click="$router.go(-1)"
           flat
           square
@@ -14,45 +13,21 @@
         <q-toolbar-title class="absolute-center">
           {{ title }}
         </q-toolbar-title>
+        <q-btn
+          flat
+          to="/auth"
+          square
+          color="white"
+          icon="account_circle"
+          label="Login"
+          class="absolute-right"
+        />
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
-    <q-footer
-      v-if="$route.fullPath.includes('/chat')"
-      class="row justify-center bg-white"
-    >
-      <q-toolbar
-        class="q-pl-none col-xs-12 col-sm-8 col-md-6 col-lg-5 bg-primary"
-      >
-        <q-toolbar-title class="row justify-center">
-          <q-input
-            outlined
-            square
-            bg-color="white"
-            v-model="newMessage"
-            placeholder="Write a message..."
-            class="full-width"
-          >
-            <template v-slot:append>
-              <q-icon
-                v-if="newMessage !== ''"
-                name="close"
-                @click="newMessage = ''"
-                class="cursor-pointer"
-              />
-            </template>
-
-            <template v-slot:after>
-              <q-btn round dense flat icon="send" color="white" />
-            </template>
-          </q-input>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
   </q-layout>
 </template>
 
@@ -61,10 +36,6 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   components: {},
-
-  data() {
-    return { newMessage: '' };
-  },
 
   computed: {
     title() {
--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -1,24 +1,54 @@
 <template>
-  <q-page class="row justify-center items-end">
-    <div class="q-pa-md row justify-center col-12">
-      <div style="width: 100%; max-width: 400px">
-        <q-chat-message name="me" :text="['hey, how are you?']" sent />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
-        <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
+  <q-page class="column">
+    <q-banner
+      inline-actions
+      class="text-white bg-grey-4 text-black text-center"
+    >
+      User is currently offline
+    </q-banner>
+    <div class="col q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px" class="column justify-end">
+        <q-chat-message
+          v-for="message of messages"
+          :key="message.text"
+          :name="message.from"
+          :text="[message.text]"
+          :sent="message.from === 'me' ? true : false"
+        />
       </div>
     </div>
+    <q-footer
+      v-if="$route.fullPath.includes('/chat')"
+      class="row justify-center bg-white"
+    >
+      <q-toolbar
+        class="q-pl-none col-xs-12 col-sm-8 col-md-6 col-lg-5 bg-primary"
+      >
+        <q-toolbar-title class="row justify-center">
+          <q-form @submit="sendMessage" class="full-width">
+            <q-input
+              outlined
+              square
+              bg-color="white"
+              v-model="newMessage"
+              placeholder="Write a message..."
+            >
+              <!-- <template v-slot:append>
+                <q-icon
+                  v-if="newMessage !== ''"
+                  name="close"
+                  @click="newMessage = ''"
+                  class="cursor-pointer"
+                />
+              </template> -->
+              <template v-slot:after>
+                <q-btn round dense flat icon="send" color="white" />
+              </template>
+            </q-input>
+          </q-form>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
@@ -29,7 +59,33 @@ export default defineComponent({
   data() {
     return {
       newMessage: '',
+      messages: [
+        {
+          text: 'Hi Michel, you doing fine?',
+          from: 'me',
+        },
+        {
+          text: 'Thx George, I am doing just great',
+          from: 'them',
+        },
+        {
+          text: 'Awesome, Michel!',
+          from: 'me',
+        },
+      ],
     };
+  },
+  methods: {
+    sendMessage() {
+      console.log('hey!');
+      if (!/^\s*$/.test(this.newMessage)) {
+        this.messages.push({
+          text: this.newMessage,
+          from: 'me',
+        });
+        this.newMessage = '';
+      }
+    },
   },
 });
 </script>
